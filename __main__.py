acfc8f2469a49742cf0e768075272787e5285e65
DimonBor <dimonbor200321@gmail.com>
2025-01-30 22:42:16 +0200

    cookie file fix

--- __main__.py
+++ __main__.py
@@ -26,7 +26,7 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
-    'cookies': 'cookies.txt'
+    'cookiefile': '/app/cookies.txt'
 }
 
 ffmpeg_options = {
